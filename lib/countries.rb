bf508faf9a179643a4004641fa67735ae0cb4690
Tirzah P. Howard <tp.howard47@gmail.com>
2020-11-07 22:50:46 +0000

    modify countries class

--- lib/countries.rb
+++ lib/countries.rb
@@ -1,4 +1,14 @@
 class Countries
- attr_accessor :country_code, :slug, :new_confirmed, :total_confirmed, :new_deaths, :total_deaths, :new_recovered, :total_recovered, :date
 
+ attr_accessor :country_code, :slug, :new_confirmed, :total_confirmed, :new_deaths, :total_deaths, :new_recovered, :total_recovered, :date
+ @@all = []
+
+ def initialize
+   @@all << self 
+ end
+
+ def self.all
+   @@all
+ end
+ 
 end
