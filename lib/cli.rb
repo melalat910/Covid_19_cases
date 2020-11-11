585310f8809c19a1d96bda34d7a3e895d5488075
Tirzah P. Howard <tp.howard47@gmail.com>
2020-11-11 05:44:39 +0000

    add names of countries to cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,28 +4,30 @@ class CLI
   puts "Welcome!"
   API.fetch_countries
   self.countries
-  self.global
  end
-def countries
-  puts "Would you like to see info on Covid-19 impact by country?"
-  puts "Type 'yes' to continue or press any other key to exit"
 
-  user_input = gets.strip.downcase
-  #if user says yes
-  if user_input =="yes" || user_input == "y"
-  puts " Choose a country to find information about new and total confirmed deaths and recoveries of Covid-19"
+ def countries
+
+   puts "Would you like to see info on Covid-19 impact by country?"
+   puts "Type 'yes' to continue or press any other key to exit"
+
+   user_input = gets.strip.downcase
+   #if user says yes
+   if user_input =="yes" || user_input == "y"
+   puts "Choose a country to find info on new and total confirmed deaths and recoveries of Covid-19."
     #display the list
-  end
+   display_list_of_countries
+   end
 
-end
-def global
-  puts "Would you like to see info on the global impact of Covid-19?"
-  puts "Type 'yes' to continue or press any other key to exit"
+ end
 
-  user_input = gets.strip.downcase
-  #if user says yes
-  if user_input =="yes" || user_input == "y"
-    puts "continue"
+  def display_list_of_countries
+   puts "All the countries"
+   #access all the countries
+   #binding.pry
+   #print each one out
+   Countries.all.each do |countries|
+   puts countries.slug
 
   end
  end
